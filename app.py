3fcd373b755ca9fdb068b7fe960f30bbea481e8b
alexnesov <alexandrenesovic@gmail.com>
2020-12-21 15:22:58 +0100

    two fields + nasdaq

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
 from flask import Flask, Response, render_template, request
 import json
 from wtforms import TextField, Form
+import pandas as pd
+
 
 app = Flask(__name__)
 
 
 class SearchForm(Form):
-    autocomp = TextField('Insert City', id='city_autocomplete')
-    country = TextField('Insert Country', id='city_autocomplete')
+    stock = TextField('Insert Stock', id='stock_autocomplete')
+    country = TextField('Insert Country', id='stock_autocomplete')
 
 
 @app.route('/_autocomplete', methods=['GET'])
 def autocomplete():
-    cities = ["Olongapo City",
-              "Angeles City",
-              "Manila",
-              "Makati",
-              "Pasig",
-              "Davao",
-              "Cebu",
-              "Quezon City",
-              "Taguig"]
-    print(cities)
-    return Response(json.dumps(cities), mimetype='application/json')
+    nasdaq = list(pd.read_csv('utils/nasdaq_list.csv').iloc[:, 0])
+
+    print(nasdaq)
+    return Response(json.dumps(nasdaq), mimetype='application/json')
+
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = SearchForm(request.form)
+    return render_template("search.html", form=form)
+
 
 
 @app.route("/submit", methods=['POST'])
@@ -30,7 +34,7 @@ def getUserInput():
 
     form = SearchForm(request.form)
     # get the city value from the form
-    city = form.autocomp.data
+    city = form.stock.data
     print(city)
     return render_template("search.html", form=form)
 
@@ -44,11 +48,6 @@ def getCountryInput():
     print(country)
     return render_template("search.html", form=form)
 
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    form = SearchForm(request.form)
-    return render_template("search.html", form=form)
 
 
 if __name__ == '__main__':
