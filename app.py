9184ce900d4a00640278d1df4d18a5cd45341f37
alexnesov <alexandrenesovic@gmail.com>
2020-12-21 13:18:40 +0100

    multiple fields + input in var

--- app.py
+++ app.py
@@ -4,44 +4,52 @@ from wtforms import TextField, Form
 
 app = Flask(__name__)
 
+
 class SearchForm(Form):
     autocomp = TextField('Insert City', id='city_autocomplete')
+    country = TextField('Insert Country', id='city_autocomplete')
 
 
 @app.route('/_autocomplete', methods=['GET'])
 def autocomplete():
     cities = ["Olongapo City",
-          "Angeles City",
-          "Manila",
-          "Makati",
-          "Pasig",
-          "Davao",
-          "Cebu",
-          "Quezon City",
-          "Taguig"]
-    print(cities)    
+              "Angeles City",
+              "Manila",
+              "Makati",
+              "Pasig",
+              "Davao",
+              "Cebu",
+              "Quezon City",
+              "Taguig"]
+    print(cities)
     return Response(json.dumps(cities), mimetype='application/json')
 
 
-
+@app.route("/submit", methods=['POST'])
 def getUserInput():
-    """
-    1. Get City User Input when he submits (see "Confirm" in search.html)
-    2. Store in "city" variable
 
-    Not mandatory to have a function like this, it's just illustrative
-    """
+    form = SearchForm(request.form)
+    # get the city value from the form
+    city = form.autocomp.data
+    print(city)
+    return render_template("search.html", form=form)
 
-    # city = variable
-    # print(city)
-    pass 
+
+@app.route("/submitcountry", methods=['POST'])
+def getCountryInput():
+
+    form = SearchForm(request.form)
+    # get the city value from the form
+    country = form.country.data
+    print(country)
+    return render_template("search.html", form=form)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = SearchForm(request.form)
-
     return render_template("search.html", form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
